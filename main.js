c45ceb2f9ac657dc529191c382b01184bd14340f
Ahnafsaihaan <96058254+Ahnafsaihaan@users.noreply.github.com>
2023-05-06 18:53:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 song = "";
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
 
 leftWristX = 0;
 leftWristY = 0;
@@ -32,6 +34,10 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score ;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+        scoreRightWrist = results[0].pose.keypoints[10].score ;
+        console.log("scoreRightWrist = " + scoreRightWrist);
 
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
@@ -46,6 +52,47 @@ function gotPoses(results)
 function draw()
 {
     image(video, 0 , 0, 600, 500);
+  
+    fill("#ff0000");
+    stroke("#000000");
+    if (scoreLeftWrist > 0.2)
+    {
+      circle(leftWristX, leftWristY, 20);
+      inNumberLeftWristY = Number(leftWristY);
+      remove_decimal = floor(inNumberLeftWristY);
+      volume = remove_decimal/500 ;
+      document.getElementById("volume").innerHTML = "Volume = " + volume;
+      song.setVolume(volume);
+    }
+    if(scoreRightWrist > 0.2)
+    {
+    circle(rightWristX, rightWristY, 20);
+    if(rightWristY > 0 && rightWristY <= 100)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 0.5x";
+        song.rate(0.5);
+    }
+    else if(rightWristY > 100 && rightWristY <= 200)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 1x";
+        song.rate(1);
+    } 
+    else if(rightWristY > 200 && rightWristY <= 300)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 1.5x";
+        song.rate(1.5);
+    }
+    else if(rightWristY > 300 && rightWristY <= 400)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 2x";
+        song.rate(2);
+    }
+    else if(rightWristY > 400 && rightWristY <= 500)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 2.5x";
+        song.rate(2.5);
+    }
+}
 }
 
 function play()
